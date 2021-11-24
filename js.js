ff3bf3e548a4b0f96abbfbd4b07b4571026378f2
huntercobra18 <85169056+huntercobra18@users.noreply.github.com>
2021-11-24 21:33:47 +0100

    Update js.js

--- js.js
+++ js.js
@@ -82,22 +82,3 @@ particlesJS("particles-js", {
   
   "retina_detect": true
 });
-
-
-/* ---- stats.js config ---- */
-
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
